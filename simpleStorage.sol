26a717f9f9aff9e5811d7ae02be425c98a02fde0
"brian chesang" <"brianchesang@gmail.com">
2021-12-03 10:58:34 +0300

    commit

--- simpleStorage.sol
+++ simpleStorage.sol
@@ -1,7 +1,19 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0 ; //this defines the version  of solidity that we'll be using
 
 contract Storage{
     uint256 public number ; //variable initialization 
+
+    struct people{
+        string name;
+        uint256 number;
+    }// this is a struct datatype whichcan store more than onetype of variableas one group 
+    mapping(string => uint256) public nameToNumber;//this is a map type which will take in a string and give us a number in return.
+
+    //to store the data from the truct i'll use an array
+     people [] public person;  // this is an array of type public and is called person
+
+
     function store(uint256 _number) public{
         number = _number;
     }// this is a function that takes a variable of type unit256 and it allows for the value of number to be changed.
@@ -9,4 +21,10 @@ contract Storage{
     function retrieve() public view returns(uint256){
         return(number);
     }// This function allws us to be able to read the value of number but not make any changes on it thus the "view"
+
+    function addPerson(string memory _name, uint256  _number) public{
+        person.push(people({name : _name , number : _number}));
+        nameToNumber[_name] = _number;
+
+    }
 }//defining a contract
