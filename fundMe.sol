b88264539b448b0e9aa762d5eb6ed1fb9bc0c55d
"brian chesang" <"brianchesang@gmail.com">
2021-12-05 18:35:23 +0300

    commit

--- fundMe.sol
+++ fundMe.sol
@@ -1,10 +1,19 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.6.6<= 0.9.0 ;
-import"@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+pragma solidity >=0.6.6 <0.9.0 ;
+import"@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract fundMe{
     mapping(address => uint256) public addressToAmountFunded;
     function fund() public payable {
          addressToAmountFunded[msg.sender] += msg.value;
     }// This function allows us to fund this contract.
+
+    function getVersion() public view returns(uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        return priceFeed.version();
+    }
+
+    function getPrice() public view returns(uint256){
+        
+    }
 }
